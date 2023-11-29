ef36df6c335c446e6382580a0b39c44a5275e1e5
hydeandjek <hydeandjek@naver.com>
2023-11-29 10:46:54 +0900

    #9. JWT 토큰 인증을 활용한 할일 수정 및 삭제

--- src/main/java/com/example/todo/todoapi/api/TodoController.java
+++ src/main/java/com/example/todo/todoapi/api/TodoController.java
@@ -72,6 +72,7 @@ public class TodoController {
     // 할 일 삭제 요청
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteTodo(
+            @AuthenticationPrincipal TokenUserInfo userInfo,
             @PathVariable("id") String todoId
     ) {
         log.info("/api/todos/{} DELETE request!", todoId);
@@ -86,7 +87,7 @@ public class TodoController {
         }
 
         try {
-            TodoListResponseDTO responseDTO = todoService.delete(todoId);
+            TodoListResponseDTO responseDTO = todoService.delete(todoId, userInfo.getUserId());
             return ResponseEntity.ok().body(responseDTO);
         } catch (Exception e) {
             return ResponseEntity
@@ -97,6 +98,7 @@ public class TodoController {
     // 할 일 수정하기
     @RequestMapping(method = {RequestMethod.PATCH, RequestMethod.PUT})
     public ResponseEntity<?> updateTodo(
+            @AuthenticationPrincipal TokenUserInfo userInfo,
             @Validated @RequestBody TodoModifyRequestDTO requestDTO,
             BindingResult result,
             HttpServletRequest request
@@ -109,7 +111,7 @@ public class TodoController {
         log.info("modifying dto: {}", requestDTO);
 
         try {
-            TodoListResponseDTO responseDTO = todoService.update(requestDTO);
+            TodoListResponseDTO responseDTO = todoService.update(requestDTO, userInfo.getUserId());
             return  ResponseEntity.ok().body(responseDTO);
         } catch (RuntimeException e) {
             return  ResponseEntity.internalServerError().body(TodoListResponseDTO.builder()
